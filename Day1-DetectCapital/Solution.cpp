813f75b011902d7f7a52114308e72e363ca7e43e
Gaganpreet Kaur Kalsi <54144759+GaganpreetKaurKalsi@users.noreply.github.com>
2020-08-02 12:01:32 +0530

    Create Solution.cpp

---
+++ Day1-DetectCapital/Solution.cpp
@@ -0,0 +1,50 @@
+// The program may seem to be a bit lengthy but the complexity is comparitively less
+// Runtime: 0 ms
+// Memory Usage: 6.2 MB
+
+#include<bits/stdc++.h>
+#include<string.h>
+#include<cstring>
+using namespace std;
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word[0]== toupper(word[0])){
+            int flag;
+            if( word[1] == toupper(word[1]) ) flag=0;
+            else flag=1;
+            if (flag==0){
+                for(int i=2;i<word.length();i++){
+                    if(word[i] != toupper(word[i])){
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else{
+                for(int i=2;i<word.length();i++){
+                    if(word[i] == toupper(word[i])){
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        else{
+            for(int i=1;i<word.length();i++){
+                if(word[i] == toupper(word[i])){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+};
+
+int main()
+{
+    string str = "gagan preet";
+    Solution obj;
+    cout<<obj.detectCapitalUse(str);
+}
