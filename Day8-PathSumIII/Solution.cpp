7188cb3ab5ad11cad35abaf511000873036a9070
Gaganpreet Kaur Kalsi <54144759+GaganpreetKaurKalsi@users.noreply.github.com>
2020-08-15 12:43:10 +0530

    Create Solution.cpp

---
+++ Day8-PathSumIII/Solution.cpp
@@ -0,0 +1,18 @@
+// Runtime: 24 ms
+// Memory Usage: 15.8 MB
+
+class Solution {
+    int pathSum_a(TreeNode* root, int sum){
+        if(root == nullptr) return 0;
+        int res = 0;
+        if(root->val == sum) res++;
+        res += pathSum_a(root->left, sum-root->val);
+        res += pathSum_a(root->right, sum-root->val);
+        return res;
+    }
+public:
+    int pathSum(TreeNode* root, int sum) {
+        if(root == nullptr) return 0;
+        return pathSum(root->left, sum) + pathSum_a(root, sum) + pathSum(root->right, sum);
+    }
+};
